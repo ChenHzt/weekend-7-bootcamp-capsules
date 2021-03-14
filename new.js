c40305026c521a9bf0252552612fdd471e597c84
ChenHzt <chenhzt@gmail.com>
2021-03-14 12:09:25 +0200

    add refresh option

--- new.js
+++ new.js
@@ -4,7 +4,6 @@ const state = {
     currentFilter: 'id',
     currentSearchMode: 'id',
     tableRows: new Map(),
-    // usersInDisplay=,
 }
 
 class APIHandler {
@@ -53,7 +52,6 @@ function User(argsmap) {
     this.hobby = argsmap.hobby;
     this.city = argsmap.city;
     this.gender = argsmap.gender;
-
 }
 
 User.prototype = {
@@ -109,11 +107,10 @@ User.prototype = {
 
 function UserCRUD() {
     this.users = new Map();
-  
 }
 
 UserCRUD.prototype = {
-    async getDataFromLocalStorage(){
+    async getDataFromLocalStorage() {
         let temp = JSON.parse(localStorage.getItem('allUsers'));
         temp = temp.map((elem) => [elem[0], new User(elem[1])]);
         this.users = new Map(temp);
@@ -175,201 +172,218 @@ UserCRUD.prototype = {
 
 const usersCrud = new UserCRUD();
 
-const deleteUser = (userId) => {
-    state.tableRows.get(userId).remove();
-    state.tableRows.delete(userId);
-    usersCrud.deleteUser(userId);
+function AppUi() {
+    this.initializeUi();
 }
 
-const switchButtons = (tr) => {
-    const toDelete = Array.from(tr.querySelectorAll(`.btn[data-active='true']`));
-    const toDisplay = Array.from(tr.querySelectorAll(`.btn[data-active='false']`));
-    toDelete.forEach((btn) => btn.setAttribute('data-active', 'false'));
-    toDisplay.forEach((btn) => btn.setAttribute('data-active', 'true'));
-}
+AppUi.prototype = {
+    async initializeUi() {
+        if (localStorage.getItem('allUsers') === null || localStorage.getItem('allUsers') === []) {
+            await usersCrud.createUsersDataset();
+            localStorage.setItem('allUsers', JSON.stringify([...usersCrud.users]));
+        }
+        else {
+            usersCrud.getDataFromLocalStorage();
+        }
+        this.addUsersToTable(usersCrud.sortUsers(state.currentFilter));
+        this.setToolbar();
+        this.setFilterListeners();
+        document.querySelector('.refresh-btn').addEventListener('click', (event) => this.refreshList());
+    },
+    updateLocalStorage() {
+        localStorage.setItem('allUsers', JSON.stringify([...usersCrud.users]));
+    },
+    deleteUser(userId) {
+        state.tableRows.get(userId).remove();
+        state.tableRows.delete(userId);
+        usersCrud.deleteUser(userId);
+        AppUi.prototype.updateLocalStorage();
+    },
 
-const editUser = (userId) => {
-    const tr = state.tableRows.get(userId);
-    const userData = usersCrud.searchUserById(userId);
-    const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
-    console.log(fields);
-    const obj = {};
-    fields.forEach((field) => {
-        const columnType = field.getAttribute('data-column-type');
-        field.innerHTML = '';
-        const input = document.createElement('input');
-        input.value = userData[columnType];
-        input.setAttribute('data-column-type', columnType);
-        field.appendChild(input);
-    });
-    switchButtons(tr);
-}
+    switchButtons(tr) {
+        const toDelete = Array.from(tr.querySelectorAll(`.btn[data-active='true']`));
+        const toDisplay = Array.from(tr.querySelectorAll(`.btn[data-active='false']`));
+        toDelete.forEach((btn) => btn.setAttribute('data-active', 'false'));
+        toDisplay.forEach((btn) => btn.setAttribute('data-active', 'true'));
+    },
 
-const cancel = (userId) => {
-    const tr = state.tableRows.get(userId);
-    const userData = usersCrud.searchUserById(userId);
-    const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
-    console.log(fields);
-    fields.forEach((field) => {
-        const columnType = field.getAttribute('data-column-type');
-        field.innerHTML = userData[columnType];
-        if (columnType === 'gender')
-            field.innerHTML = `<i class="fas fa-${userData[columnType].toLowerCase() === 'female' ? 'female' : 'male'}"></i>`;
-
-
-    });
-    switchButtons(tr);
-}
-
-const confirm = (userId) => {
-    const tr = state.tableRows.get(userId);
-    const userData = usersCrud.searchUserById(userId);
-    const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
-    console.log(fields);
-    const isValidData = fields.every((field) => {
-        const input = field.querySelector('input[data-column-type]');
-        const columnType = field.getAttribute('data-column-type');
-        return userData.validProperty(columnType, input.value)
-        // field.innerHTML = input.value;
-        // if (columnType === 'gender')
-        //     field.innerHTML = `<i class="fas fa-${input.value === 'Female' ? 'female' : 'male'}"></i>`;
-    });
-    if (isValidData) {
+    editUser(userId) {
+        const tr = state.tableRows.get(userId);
+        const userData = usersCrud.searchUserById(userId);
+        const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
+        console.log(fields);
+        const obj = {};
         fields.forEach((field) => {
-            const input = field.querySelector('input[data-column-type]');
             const columnType = field.getAttribute('data-column-type');
-            userData.setProperty(columnType, input.value)
+            field.innerHTML = '';
+            const input = document.createElement('input');
+            input.value = userData[columnType];
+            input.setAttribute('data-column-type', columnType);
+            field.appendChild(input);
+        });
+        AppUi.prototype.switchButtons(tr);
+        AppUi.prototype.updateLocalStorage();
+
+    },
+
+    cancel(userId) {
+        const tr = state.tableRows.get(userId);
+        const userData = usersCrud.searchUserById(userId);
+        const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
+        console.log(fields);
+        fields.forEach((field) => {
+            const columnType = field.getAttribute('data-column-type');
             field.innerHTML = userData[columnType];
             if (columnType === 'gender')
                 field.innerHTML = `<i class="fas fa-${userData[columnType].toLowerCase() === 'female' ? 'female' : 'male'}"></i>`;
         });
-        usersCrud.editUser(userData);
-        switchButtons(tr);
-    }
-    else fields.forEach((field) => {
-        const columnType = field.getAttribute('data-column-type');
-        const input = field.querySelector('input[data-column-type]');
-        if (!userData.validProperty(columnType, input.value))
-            input.style.boxShadow = '0 0 5pt 2pt red';
-        input.style.outlineWidth = '0px';
+        AppUi.prototype.switchButtons(tr);
+    },
 
-    })
-}
+    confirm(userId) {
+        const tr = state.tableRows.get(userId);
+        const userData = usersCrud.searchUserById(userId);
+        const fields = Array.from(tr.querySelectorAll('td[data-column-type]'));
+        const isValidData = fields.every((field) => {
+            const input = field.querySelector('input[data-column-type]');
+            const columnType = field.getAttribute('data-column-type');
+            return userData.validProperty(columnType, input.value)
+        });
+        if (isValidData) {
+            fields.forEach((field) => {
+                const input = field.querySelector('input[data-column-type]');
+                const columnType = field.getAttribute('data-column-type');
+                userData.setProperty(columnType, input.value)
+                field.innerHTML = userData[columnType];
+                if (columnType === 'gender')
+                    field.innerHTML = `<i class="fas fa-${userData[columnType].toLowerCase() === 'female' ? 'female' : 'male'}"></i>`;
+            });
+            usersCrud.editUser(userData);
+            AppUi.prototype.switchButtons(tr);
+        }
+        else fields.forEach((field) => {
+            const columnType = field.getAttribute('data-column-type');
+            const input = field.querySelector('input[data-column-type]');
+            if (!userData.validProperty(columnType, input.value))
+                input.style.boxShadow = '0 0 5pt 2pt red';
+            else
+                input.style.boxShadow = 'none';
+            input.style.outlineWidth = '0px';
 
-const btnClicked = (event) => {
-    const btn = event.target;
-    const userId = parseInt(btn.getAttribute('data-user-id'));
-    const tr = state.tableRows.get(userId);
-
-    switch (btn.getAttribute('data-btn-type')) {
-        case 'edit':
-            editUser(userId);
-            break;
-        case 'delete':
-            deleteUser(userId);
-            break;
-        case 'cancel':
-            cancel(userId);
-            break;
-        case 'confirm':
-            confirm(userId);
-            break;
-        default:
-            throw new Error(`unnkown button of type "${btn.getAttribute('data-btn-type')}"`);
-    }
-}
-
-const createTableRow = (user) => {
-    const tr = document.createElement('tr');
-    const buttonsTd = document.createElement('td');
-
-    const createTd = (content, type) => {
-        const td = document.createElement('td');
-        td.innerHTML = content;
-        td.setAttribute('data-column-type', type);
-        tr.appendChild(td);
-    }
-
-    const createButton = (btnType, isActive) => {
-        const btn = document.createElement('button');
-        btn.innerHTML = btnType;
-        btn.classList.add('btn');
-        btn.setAttribute('data-btn-type', btnType);
-        btn.setAttribute('data-active', isActive);
-        btn.setAttribute('data-user-id', user.id);
-        btn.addEventListener('click', btnClicked);
-        buttonsTd.appendChild(btn);
-    }
-    const td = document.createElement('td');
-    td.innerHTML = user.id;
-    tr.appendChild(td);
-    console.log(user);
-    createTd(user.firstName, 'firstName');
-    createTd(user.lastName, 'lastName');
-    createTd(user.capsule, 'capsule');
-    createTd(user.age, 'age');
-    createTd(user.city, 'city');
-    createTd(`<i class="fas fa-${user.gender === 'Female' ? 'female' : 'male'}"></i>`, 'gender');
-    createTd(user.hobby, 'hobby');
-
-    createButton('edit', 'true');
-    createButton('delete', 'true');
-    createButton('cancel', 'false');
-    createButton('confirm', 'false');
-
-    tr.appendChild(buttonsTd);
-    state.tableRows.set(user.id, tr);
-    return tr;
-}
-
-
-const addUsersToTable = (usersInDisplay) => {
-    console.log(usersInDisplay);
-    table.innerHTML = '';
-
-    for (let i = 0; i < usersInDisplay.length; i++) {
-        const tr = createTableRow(usersInDisplay[i]);
-        table.appendChild(tr);
-    }
-}
-
-const setToolbar = () => {
-    document.querySelector('#search-by').addEventListener('change', (event) => {
-        state.currentSearchMode = event.target.value;
-        document.querySelector('#search-input').value = '';
-        console.log(state.currentSearchMode);
-    });
-
-    document.querySelector('#search-input').addEventListener('input', (event) => {
-        console.log(usersCrud.search(state.currentSearchMode, event.target.value));
-        if (!event.target.value) addUsersToTable(usersCrud.sortUsers(state.currentFilter));
-        addUsersToTable(usersCrud.search(state.currentSearchMode, event.target.value));
-    });
-}
-
-const setFilterListeners = () => {
-    const tableHeaders = Array.from(document.querySelectorAll('th[data-column]'));
-    tableHeaders.forEach((header) => {
-        header.addEventListener('click', (event) => {
-            state.currentFilter = event.target.getAttribute('data-column')
-            addUsersToTable(usersCrud.sortUsers(state.currentFilter));
-            tableHeaders.forEach((h) => h.setAttribute('data-filter', 'false'));
-            event.target.setAttribute('data-filter', 'true');
         })
-    })
-}
+        AppUi.prototype.updateLocalStorage();
 
-const init = async () => {
-    if (localStorage.getItem('allUsers') === null || localStorage.getItem('allUsers') === []) {
+    },
+
+    btnClicked(event) {
+        const btn = event.target;
+        const userId = parseInt(btn.getAttribute('data-user-id'));
+        const tr = state.tableRows.get(userId);
+        switch (btn.getAttribute('data-btn-type')) {
+            case 'edit':
+                AppUi.prototype.editUser(userId);
+                break;
+            case 'delete':
+                AppUi.prototype.deleteUser(userId);
+                break;
+            case 'cancel':
+                AppUi.prototype.cancel(userId);
+                break;
+            case 'confirm':
+                AppUi.prototype.confirm(userId);
+                break;
+            default:
+                throw new Error(`unnkown button of type "${btn.getAttribute('data-btn-type')}"`);
+        }
+    },
+
+    createTableRow(user) {
+        const tr = document.createElement('tr');
+        const buttonsTd = document.createElement('td');
+
+        const createTd = (content, type) => {
+            const td = document.createElement('td');
+            td.innerHTML = content;
+            td.setAttribute('data-column-type', type);
+            tr.appendChild(td);
+        }
+
+        const createButton = (btnType, isActive) => {
+            const btn = document.createElement('button');
+            btn.innerHTML = btnType;
+            btn.classList.add('btn');
+            btn.setAttribute('data-btn-type', btnType);
+            btn.setAttribute('data-active', isActive);
+            btn.setAttribute('data-user-id', user.id);
+            btn.addEventListener('click', this.btnClicked);
+            buttonsTd.appendChild(btn);
+        }
+        const td = document.createElement('td');
+        td.innerHTML = user.id;
+        tr.appendChild(td);
+        createTd(user.firstName, 'firstName');
+        createTd(user.lastName, 'lastName');
+        createTd(user.capsule, 'capsule');
+        createTd(user.age, 'age');
+        createTd(user.city, 'city');
+        createTd(`<i class="fas fa-${user.gender === 'Female' ? 'female' : 'male'}"></i>`, 'gender');
+        createTd(user.hobby, 'hobby');
+
+        createButton('edit', 'true');
+        createButton('delete', 'true');
+        createButton('cancel', 'false');
+        createButton('confirm', 'false');
+
+        tr.appendChild(buttonsTd);
+        state.tableRows.set(user.id, tr);
+        return tr;
+    },
+
+    addUsersToTable(usersInDisplay) {
+        console.log(usersInDisplay);
+        table.innerHTML = '';
+
+        for (let i = 0; i < usersInDisplay.length; i++) {
+            const tr = this.createTableRow(usersInDisplay[i]);
+            table.appendChild(tr);
+        }
+    },
+
+    setToolbar() {
+        document.querySelector('#search-by').addEventListener('change', (event) => {
+            state.currentSearchMode = event.target.value;
+            document.querySelector('#search-input').value = '';
+            console.log(state.currentSearchMode);
+        });
+
+        document.querySelector('#search-input').addEventListener('input', (event) => {
+            console.log(usersCrud.search(state.currentSearchMode, event.target.value));
+            if (!event.target.value) AppUi.prototype.addUsersToTable(usersCrud.sortUsers(state.currentFilter));
+            else AppUi.prototype.addUsersToTable(usersCrud.search(state.currentSearchMode, event.target.value));
+        });
+    },
+
+    setFilterListeners() {
+        const tableHeaders = Array.from(document.querySelectorAll('th[data-column]'));
+        tableHeaders.forEach((header) => {
+            header.addEventListener('click', (event) => {
+                state.currentFilter = event.target.getAttribute('data-column')
+                addUsersToTable(usersCrud.sortUsers(state.currentFilter));
+                tableHeaders.forEach((h) => h.setAttribute('data-filter', 'false'));
+                event.target.setAttribute('data-filter', 'true');
+            })
+        })
+    },
+
+    async refreshList() {
+        localStorage.removeItem('allUsers');
         await usersCrud.createUsersDataset();
         localStorage.setItem('allUsers', JSON.stringify([...usersCrud.users]));
+        console.log(usersCrud.users);
+        console.log(usersCrud.sortUsers(state.currentFilter));
+        AppUi.prototype.addUsersToTable(usersCrud.sortUsers(state.currentFilter));
     }
-    else {
-        usersCrud.getDataFromLocalStorage();
-    }
-    addUsersToTable(usersCrud.sortUsers(state.currentFilter));
-    setToolbar();
-    setFilterListeners();
+
 }
 
-init();
+const appUi = new AppUi();
